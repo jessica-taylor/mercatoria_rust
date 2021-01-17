f8b023c98ccdcbce2c6c7c475033e76408e2fbb2
Jessica Taylor <jessi.liu.taylor@gmail.com>
2021-01-17 16:26:19 -0500

    to public key

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,6 +1,6 @@
 use std::convert::TryInto;
 
-use openssl::{hash::MessageDigest, pkey::Private, rsa::Rsa, sign::{Signer, Verifier}};
+use openssl::{hash::MessageDigest, pkey::{PKey, Public, Private}, rsa::Rsa, sign::{Signer, Verifier}};
 use sha2::{Sha256, Digest};
 
 
@@ -13,8 +13,18 @@ fn hash_of_bytes(bs: &[u8]) -> HashCode {
     hasher.finalize().as_slice().try_into().expect("digest has wrong length")
 }
 
-fn gen_key_pair() -> Rsa<Private> {
+fn gen_private_key() -> Rsa<Private> {
     Rsa::generate(2048).unwrap()
 }
 
+fn to_public_key(private: &Rsa<Private>) -> Rsa<Public> {
+    Rsa::public_key_from_pem(private.private_key_to_pem().unwrap().as_slice()).unwrap()
+}
+
+fn sign(key: &Rsa<Private>, msg: &[u8]) -> Vec<u8> {
+    let pkey = PKey::from_rsa(key.clone()).unwrap();
+    let mut signer = Signer::new(MessageDigest::sha256(), &pkey).unwrap();
+    signer.update(msg).unwrap();
+    signer.sign_to_vec().unwrap()
+}
 
