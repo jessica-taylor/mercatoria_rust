f568ee392eb888ecd361c7e03271e8618c85757c
Jessica Taylor <jessi.liu.taylor@gmail.com>
2021-01-18 14:40:21 -0500

    hashput maphashlookup

--- src/hashlookup.rs
+++ src/hashlookup.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 
-use crate::crypto::HashCode;
+use crate::crypto::{HashCode, hash_of_bytes};
 
 
 
@@ -17,11 +17,23 @@ pub struct MapHashLookup {
     map: BTreeMap<HashCode, Vec<u8>>,
 }
 
+impl MapHashLookup {
+    pub fn new() -> MapHashLookup {
+        MapHashLookup {map: BTreeMap::new()}
+    }
+}
+
 impl HashLookup for MapHashLookup {
     fn hash_lookup(&self, hash: HashCode) -> Option<Vec<u8>> {
         match self.map.get(&hash) {
             None => None,
             Some(x) => Some(x.clone())
         }
+    }
+}
+
+impl HashPut for MapHashLookup {
+    fn hash_put(&mut self, bs: Vec<u8>) {
+        self.map.insert(hash_of_bytes(&bs), bs);
     }
 }
