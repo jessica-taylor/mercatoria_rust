caccf4ccafe069df42b1856a38bd0e958b2b34f7
Jessica Taylor <jessi.liu.taylor@gmail.com>
2021-02-11 14:19:46 -0500

    tree info

--- src/account_construction.rs
+++ src/account_construction.rs
@@ -1,7 +1,8 @@
-use crate::blockdata::{DataNode, MainBlock, MainBlockBody, PreSignedMainBlock, QuorumNode};
+use crate::blockdata::{DataNode, MainBlock, MainBlockBody, PreSignedMainBlock, QuorumNode, QuorumNodeBody};
 use crate::crypto::{hash, path_to_hash_code, Hash, HashCode};
 use crate::hashlookup::HashLookup;
 use crate::hex_path::{bytes_to_path, HexPath};
+
 
 fn children_paths_well_formed<N>(children: &Vec<(HexPath, N)>) -> bool {
     for i in 0..children.len() {
@@ -15,4 +16,26 @@ fn children_paths_well_formed<N>(children: &Vec<(HexPath, N)>) -> bool {
 
 fn data_node_well_formed(dn: &DataNode) -> bool {
     children_paths_well_formed(&dn.children) && !(dn.children.len() <= 1 && dn.value.is_none())
+}
+
+struct TreeInfo {
+    fee: u128,
+    gas: u128,
+    new_nodes: u64,
+    prize: u128,
+    stake: u128,
+    new_transactions: u64,
+    new_quorums: u64
+}
+
+fn cached_tree_info(qnb: &QuorumNodeBody) -> TreeInfo {
+    TreeInfo {
+        fee: qnb.total_fee,
+        gas: qnb.total_gas,
+        new_nodes: qnb.new_nodes,
+        prize: qnb.total_prize,
+        stake: qnb.total_stake,
+        new_transactions: 0,
+        new_quorums: 0
+    }
 }
