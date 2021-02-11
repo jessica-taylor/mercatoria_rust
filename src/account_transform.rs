1572fea029e4842abd06a636e8a1e9421d42aa59
Jessica Taylor <jessi.liu.taylor@gmail.com>
2021-02-11 16:20:21 -0500

    account transform

--- src/account_transform.rs
+++ src/account_transform.rs
@@ -32,22 +32,22 @@ impl<T> TypedDataField<T> {
     fn from_path(path: HexPath) -> TypedDataField<T> {
         TypedDataField { path: path, phantom: PhantomData }
     }
+}
 
-    fn balance() -> TypedDataField<u128> {
-        TypedDataField::from_path(bytes_to_path(b"balance"))
-    }
-    fn stake() -> TypedDataField<u128> {
-        TypedDataField::from_path(bytes_to_path(b"balance"))
-    }
-    fn public_key() -> TypedDataField<Vec<u8>> {
-        TypedDataField::from_path(bytes_to_path(b"public_key"))
-    }
+pub fn field_balance() -> TypedDataField<u128> {
+    TypedDataField::from_path(bytes_to_path(b"balance"))
+}
+pub fn field_stake() -> TypedDataField<u128> {
+    TypedDataField::from_path(bytes_to_path(b"balance"))
+}
+pub fn field_public_key() -> TypedDataField<Vec<u8>> {
+    TypedDataField::from_path(bytes_to_path(b"public_key"))
 }
 
 
 /// A context providing operations related to transforming accounts (e.g.
 /// running actions).
-struct AccountTransform<'a, HL : HashLookup> {
+pub struct AccountTransform<'a, HL : HashLookup> {
     pub hl: &'a HL,
     pub is_initializing: bool,
     pub this_account: HashCode,
@@ -72,16 +72,18 @@ impl<'a, HL : HashLookup> AccountTransform<'a, HL> {
     }
 
     /// Gets the value of a given data field.
-    fn get_data_field_bytes(&self, field_name: &HexPath) -> Result<Option<Vec<u8>>, String> {
-        match self.fields_set.get(field_name) {
-            Some(x) => Ok(Some(x.clone())),
-            None => {
-                let main = self.lookup(self.hash_last_main)?;
-                let acct_node = lookup_account(self, &main.block.body, self.this_account)?;
-                lookup_data_in_account(self, &acct_node, field_name)
+    fn get_data_field_bytes(&self, acct: HashCode, field_name: &HexPath) -> Result<Option<Vec<u8>>, String> {
+        if acct == self.this_account {
+            match self.fields_set.get(field_name) {
+                Some(x) => {
+                    return Ok(Some(x.clone()));
+                }
+                None => {}
             }
         }
-
+        let main = self.lookup(self.hash_last_main)?;
+        let acct_node = lookup_account(self, &main.block.body, self.this_account)?;
+        lookup_data_in_account(self, &acct_node, field_name)
     }
 
     /// Sets the value of a given data field.
@@ -91,8 +93,8 @@ impl<'a, HL : HashLookup> AccountTransform<'a, HL> {
     }
 
     /// Gets the value of a given typed data field.
-    fn get_data_field<T : DeserializeOwned>(&self, field: &TypedDataField<T>) -> Result<Option<T>, String> {
-        match self.get_data_field_bytes(&field.path)? {
+    fn get_data_field<T : DeserializeOwned>(&self, acct: HashCode, field: &TypedDataField<T>) -> Result<Option<T>, String> {
+        match self.get_data_field_bytes(acct, &field.path)? {
             None => Ok(None),
             Some(bs) => match rmp_serde::from_read(bs.as_slice()) {
                 Ok(val) => Ok(Some(val)),
@@ -102,8 +104,8 @@ impl<'a, HL : HashLookup> AccountTransform<'a, HL> {
     }
 
     /// Gets the value of a given typed data field, throwing an error if it is not found.
-    fn get_data_field_or_error<T : DeserializeOwned>(&self, field: &TypedDataField<T>) -> Result<T, String> {
-        match self.get_data_field(field)? {
+    fn get_data_field_or_error<T : DeserializeOwned>(&self, acct: HashCode, field: &TypedDataField<T>) -> Result<T, String> {
+        match self.get_data_field(acct, field)? {
             None => Err(format!("data field not found: {:?}", field.path)),
             Some(x) => Ok(x)
         }
@@ -113,4 +115,12 @@ impl<'a, HL : HashLookup> AccountTransform<'a, HL> {
     fn set_data_field<T : Serialize>(&mut self, field: &TypedDataField<T>, value: &T) -> Result<(), String> {
         self.set_data_field_bytes(&field.path, rmp_serde::to_vec_named(value).unwrap())
     }
+}
+
+pub fn pay_fee<'a, HL : HashLookup>(at: &mut AccountTransform<'a, HL>, fee: u128) -> Result<(), String> {
+    let bal = at.get_data_field_or_error(at.this_account, &field_balance())?;
+    if bal < fee {
+        return Err("not enough balance for fee".to_string());
+    }
+    at.set_data_field(&field_balance(), &(bal - fee))
 }
