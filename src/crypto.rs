17cbb6d8974eec658419da462699432587255aac
theseriousadult <jack@gallabytes.com>
2021-02-11 13:29:25 -0500

    fix docstrings

--- src/crypto.rs
+++ src/crypto.rs
@@ -8,7 +8,7 @@ use crate::hex_path::HexPath;
 /// A blake3 hash code.
 pub type HashCode = [u8; 32];
 
-/// A SHA256 hash code that is tagged as being a hash code of a particular serializable type.
+/// A blake3 hash code that is tagged as being a hash code of a particular serializable type.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Hash<T> {
     pub code: HashCode,
@@ -26,12 +26,12 @@ impl<T> Clone for Hash<T> {
 
 impl<T> Copy for Hash<T> {}
 
-/// Gets the SHA256 hash code of a byte array.
+/// Gets the blake3 hash code of a byte array.
 pub fn hash_of_bytes(bs: &[u8]) -> HashCode {
     blake3::hash(bs).into()
 }
 
-/// Gets the SHA256 hash of a serialiable data value.
+/// Gets the blake3 hash of a serialiable data value.
 pub fn hash<T: Serialize>(v: &T) -> Hash<T> {
     Hash {
         code: hash_of_bytes(rmp_serde::to_vec_named(v).unwrap().as_slice()),
@@ -51,7 +51,7 @@ pub fn path_to_hash_code(path: HexPath) -> HashCode {
     hc
 }
 
-/// A RSA signature that is tagged as being the signature of a particular serializable type.
+/// An ed25519 signature that is tagged as being the signature of a particular serializable type.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Signature<T> {
     pub sig: Sig,
@@ -69,12 +69,12 @@ impl<T> Clone for Signature<T> {
 
 impl<T> Copy for Signature<T> {}
 
-/// Generates a RSA private key.
+/// Generates a ed25519 private key.
 pub fn gen_private_key() -> Keypair {
     Keypair::generate(&mut thread_rng())
 }
 
-/// Signs a serializable value with a given RSA key.
+/// Signs a serializable value with a given ed25519 key.
 pub fn sign<T: Serialize, Deserialize>(key: &Keypair, msg: T) -> Signature<T> {
     Signature {
         sig: key.sign(&rmp_serde::to_vec_named(&msg).unwrap()),
