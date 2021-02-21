18ecd6222d6cb822350395cfe865a4e4a9231fcc
theseriousadult <jack@gallabytes.com>
2021-02-21 16:04:14 -0500

    set up ordering for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
-pub mod account_construction;
-pub mod account_transform;
-pub mod blockdata;
-pub mod construction;
-pub mod crypto;
-pub mod hashlookup;
 pub mod hex_path;
+
+pub mod crypto;
+
+pub mod blockdata;
+pub mod hashlookup;
 pub mod network;
+
 pub mod queries;
+
+pub mod account_transform;
+
+pub mod account_construction;
+
 pub mod verification;
+
+pub mod construction;
