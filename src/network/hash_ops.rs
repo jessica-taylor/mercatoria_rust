7b434bfb663e77a9b2dee0897a92814ce8bb3e4a
Jessica Taylor <jessi.liu.taylor@gmail.com>
2021-05-24 15:19:21 -0400

    todo

--- src/network/hash_ops.rs
+++ src/network/hash_ops.rs
@@ -6,6 +6,9 @@ use super::peer_tracker::PeerTracker;
 use super::role::Role;
 use super::Network;
 use crate::crypto::{hash, xor_hash_codes, HashCode};
+use crate::hashlookup::{HashLookup, HashPut};
+use anyhow::bail;
+use async_trait::*;
 use std::collections::BTreeSet;
 use std::sync::Arc;
 
@@ -18,7 +21,8 @@ pub struct HashOps<N: Network + 'static> {
 }
 
 impl<N: Network> HashOps<N> {
-    fn new(
+    /// Creates a new `HashOps`.
+    pub fn new(
         log: Arc<Log>,
         peer_tracker: Arc<PeerTracker<N>>,
         message_sender: Arc<MessageSender<N>>,
@@ -31,10 +35,20 @@ impl<N: Network> HashOps<N> {
             query_sender,
         }
     }
-    fn hash_to_storing_peers(&self, code: HashCode) -> BTreeSet<N::Pid> {
+
+    /// Gets the peers who are most likely to be storing a data corresponding to a particular hash
+    /// code.  This uses a basic DHT algorithm.
+    pub fn hash_to_storing_peers(&self, code: HashCode) -> BTreeSet<N::Pid> {
         let mut peers: Vec<N::Pid> = self.peer_tracker.get_peers().into_iter().collect();
         peers.sort_by_key(|pid| xor_hash_codes(hash(pid).code, code));
         peers.truncate(30);
         peers.into_iter().collect()
+    }
+}
+
+#[async_trait]
+impl<N: Network + 'static + Send + Sync> HashLookup for HashOps<N> {
+    async fn lookup_bytes(&self, hash: HashCode) -> Result<Vec<u8>, anyhow::Error> {
+        bail!("TODO")
     }
 }
