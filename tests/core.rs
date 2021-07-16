dfa1af77e999c6015ca32c915391c4d6a5d8c59a
Devi Borg <devi.borg@gmail.com>
2021-07-16 09:05:25 +0200

    tests/core: call partial test_send_and_receive thru proptest and trace back failure to genesis block test

--- tests/core.rs
+++ tests/core.rs
@@ -15,6 +15,8 @@ use mercatoria_rust::state_machine::{genesis_state, get_account_state, get_main_
 use mercatoria_rust::verification::verify_valid_main_block_body;
 use proptest::prelude::*;
 
+use mercatoria_rust::queries;
+
 mod strategies;
 use strategies::*;
 
@@ -74,27 +76,38 @@ async fn test_genesis_block(
         expected_state, actual_state
     );
     verify_valid_main_block_body(&hl, &main).await.unwrap();
+    for init in inits.iter() {
+        let h = hash(&init.public_key).code;
+        let acc = queries::lookup_account(&hl, &main, h).await;
+        assert!(acc.is_ok(), "account data not accessed");
+        let acc = acc.ok().unwrap();
+        assert!(acc.is_some(), "account data not stored");
+        let acc_data = acc.unwrap().body.data_tree;
+        assert!(acc_data.is_some(), "missing data tree");
+        let _acc_data = acc_data.unwrap();
+        // TODO test account data contents
+    }
     (hl, main)
 }
 
-async fn _test_send_and_receive(
+async fn test_send_and_receive(
     hl: &mut MapHashLookup,
     keys: &BTreeMap<HashCode, Keypair>,
     start_main: &MainBlock,
-    sender_ix: u32,
-    receiver_ix: u32,
-    fee: u64,
-    amount: u64,
+    sender_ix: usize,
+    receiver_ix: usize,
+    fee: u128,
+    amount: u128,
 ) -> Result<(), anyhow::Error> {
     let start_state = get_main_state(hl, &start_main.block.body).await.unwrap();
     let accts: Vec<HashCode> = start_state.accounts.keys().cloned().collect();
-    let sender = accts[(sender_ix as usize) % accts.len()];
-    let receiver = accts[(receiver_ix as usize) % accts.len()];
+    let sender = accts[sender_ix % accts.len()];
+    let receiver = accts[receiver_ix % accts.len()];
     let (send_act, _send_info) = mk_send(
         hash(start_main),
-        fee as u128,
+        fee,
         receiver,
-        amount as u128,
+        amount,
         None,
         vec![],
         keys.get(&sender).unwrap(),
@@ -103,6 +116,8 @@ async fn _test_send_and_receive(
         .await?
         .into_unsigned();
     let _sender_new_hash = hl.put(&sender_new_node).await?;
+
+    // TODO finish the test by constructing two new main blocks, one with sends and one with receives
 
     Ok(())
     // recipient: HashCode,
@@ -161,5 +176,45 @@ proptest! {
         timestamp_ms in prop::num::i32::ANY
     ) {
         smol::block_on(test_genesis_block(&inits.0, &inits.1, timestamp_ms as i64, test_options()));
+    }
+    #[test]
+    fn proptest_transfer(
+        inits in account_inits(),
+        timestamp_ms in prop::num::i32::ANY
+    ) {
+        let account_inits = inits.0;
+        let account_keys = inits.1;
+        let (mut hl, genesis_block_body) = smol::block_on(test_genesis_block(&account_inits, &account_keys, timestamp_ms as i64, test_options()));
+        if account_inits.len() < 2 {
+            // Impossible to test transfer without at least two accounts, TODO generate in a way that avoids this
+            return Ok(());
+        }
+        let miner_keys = account_keys.get(&hash(&account_inits[0].public_key).code).unwrap();
+        let presigned_genesis_block = PreSignedMainBlock{
+            body: genesis_block_body.clone(),
+            signatures: account_inits.iter().map(|init| sign(account_keys.get(&hash(&init.public_key).code).unwrap(), genesis_block_body.clone()).clone()).collect::<Vec<_>>(),
+        };
+        let genesis_block = MainBlock{
+            block: presigned_genesis_block.clone(),
+            signature: sign(miner_keys, presigned_genesis_block),
+        };
+        let mut sender_ix: Option<usize> = None;
+        // TODO use non-deterministic index
+        for (i, acc) in account_inits.iter().enumerate() {
+            if acc.balance > 0 {
+                sender_ix = Some(i);
+            }
+        }
+        // TODO change indices to hashes since order of accounts need not be persisted
+        let sender_ix = match sender_ix{
+            None => return Ok(()),
+            Some(ix) => ix,
+        };
+        let receiver_ix = (sender_ix + 1) % account_inits.len();
+        let sender_balance = account_inits[sender_ix as usize].balance;
+        let to_send_amount = (sender_balance + 1)/2;
+        let fee = (sender_balance - to_send_amount)/2;
+        let res = smol::block_on(test_send_and_receive(&mut hl, &account_keys, &genesis_block, sender_ix, receiver_ix, fee, to_send_amount));
+        assert!(res.is_ok(), "got error: {}", res.err().unwrap())
     }
 }
